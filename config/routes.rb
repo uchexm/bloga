e6a32f5ee4595a5e6a148f7781a2c16ea22b0c57
uchexm <109084069+uchexm@users.noreply.github.com>
2023-05-09 15:51:22 +0200

    add styleseet

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'users#index'
   get 'users' => 'users#index', as: 'users'
-  get 'users/:id' => 'users#show', as: 'user'
-  get 'users/:id/posts' => 'posts#index', as: 'user_posts'
-  get 'users/:id/posts/:post_id' => 'posts#show', as: 'user_post'
+  get 'users/:id' => 'users#show', as: :user
+  get 'users/:id/posts' => 'posts#index', as: :user_posts
+  get 'users/:id/posts/:post_id' => 'posts#show', as: :user_post
 end
