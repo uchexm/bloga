3322317e25343d72612f0a2c0232054db6049a32
uchexm <109084069+uchexm@users.noreply.github.com>
2023-05-10 11:50:32 +0200

    Fix controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   resources :likes
   resources :posts
   resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'users#index'
-  get 'users' => 'users#index', as: 'users'
-  get 'users/:id' => 'users#show', as: :user
-  get 'users/:id/posts' => 'posts#index', as: :user_posts
-  get 'users/:id/posts/:post_id' => 'posts#show', as: :user_post
 end
