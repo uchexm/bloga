6c25dd17cec82fdce4e5aa527d3c3d036c24cda2
uchexm <109084069+uchexm@users.noreply.github.com>
2023-05-11 20:45:35 +0200

    fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,30 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts controller:', type: :request do
-  
-  
-describe 'GET all posts for an user' do
-  let(:user) { User.create!(name: 'Test User', post_count: 0) }
+  describe 'GET all posts for an user' do
+    let(:user) { User.create!(name: 'Test User', post_count: 0) }
 
-  it 'returns a successful response' do
-    get "/users/#{user.id}/posts"
+    it 'returns a successful response' do
+      get "/users/#{user.id}/posts"
 
-    expect(response).to be_successful
-    expect(response.body).to include('<h1>Here is a list of posts for a given user</h1>')
-    expect(response).to render_template(:index)
+      expect(response).to be_successful
+      expect(response.body).to include('<h1>Here is a list of posts for a given user</h1>')
+      expect(response).to render_template(:index)
+    end
   end
-end
 
-describe 'GET specific post for a user' do
-  let(:user) { User.create!(name: 'Test User', post_count: 0) }
+  describe 'GET specific post for a user' do
+    let(:user) { User.create!(name: 'Test User', post_count: 0) }
 
-  it 'returns a successful response' do
-    post = Post.create!(title: 'Test Post', author_id: user.id, comments_counter: 0, likes_counter: 0)
-    get "/users/#{user.id}/posts/#{post.id}"
+    it 'returns a successful response' do
+      post = Post.create!(title: 'Test Post', author_id: user.id, comments_counter: 0, likes_counter: 0)
+      get "/users/#{user.id}/posts/#{post.id}"
 
-    expect(response).to be_successful
-    expect(response.body).to include('<h1>This is a single post</h1>')
-    expect(response).to render_template(:show)
+      expect(response).to be_successful
+      expect(response.body).to include('<h1>This is a single post</h1>')
+      expect(response).to render_template(:show)
+    end
   end
-end
 end
