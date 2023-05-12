d06a156ceaa12a34e6270141ed052a49da425f2f
uchexm <109084069+uchexm@users.noreply.github.com>
2023-05-12 10:06:29 +0200

    Implement design views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,9 @@
-class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :likes
-  has_many :comments
-  after_save :update_posts_counter
-
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-
-  def update_posts_counter
-    author.increment!(:posts_counter)
+class UsersController < ApplicationController
+  def index
+    @users = User.all.order(created_at: :asc)
   end
 
-  def recent_comment
-    comments.order(created_at: :desc).limit(5)
+  def show
+    @users = User.find(params[:id])
   end
 end
