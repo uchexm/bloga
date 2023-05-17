65e62284eb5a8a6e76f96ab904d84f5db7d146f4
uc <nwakauc1@gmail.com>
2023-05-17 08:59:24 +0200

    update controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,30 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.all
+    @posts = Post.where(author_id: @user.id).includes(:comments)
   end
 
   def show
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text).merge(author_id: current_user.id, comments_counter: 0,
+                                                                       likes_counter: 0))
+
+    respond_to do |format|
+      format.html do
+        if @post.save
+          redirect_to user_post_path(current_user, @post)
+        else
+          redirect_to new_user_post_path(current_user)
+        end
+      end
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order(created_at: :asc)
+    @users = User.all
   end
 
   def show
-    @users = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 end
